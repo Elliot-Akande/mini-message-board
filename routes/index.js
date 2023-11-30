d1223b6a7ade7461a4f0914429be3966c5238a8b
Elliot Akande <elbo.lynch@gmail.com>
2023-11-30 15:58:31 +0000

    Add new message form route

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,19 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+
+router.post("/new", function (req, res, next) {
+  const { user, text } = req.body;
+  messages.push({
+    text,
+    user,
+    added: new Date(),
+  });
+
+  res.redirect("/");
+});
+
 module.exports = router;
